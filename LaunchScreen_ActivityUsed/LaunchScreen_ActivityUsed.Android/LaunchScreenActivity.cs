20d126da9cefb2647305eecc0e63c04f06520584
kurigohan <kurigohan_blog-contact@yahoo.co.jp>
2018-02-20 21:33:48 +0900

    Add project files.

---
+++ LaunchScreen_ActivityUsed/LaunchScreen_ActivityUsed.Android/LaunchScreenActivity.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Xamarin.Forms.Platform.Android;
+using Android.OS;
+using Android.Widget;
+using Android.Graphics.Drawables;
+
+namespace LaunchScreen_ActivityUsed.Droid
+{
+    [Activity(MainLauncher = true, Label = "LaunchScreen_ActivityUsed", Icon = "@mipmap/greeeen_icon", Theme = "@style/LaunchScreenTheme")]
+    public class LaunchScreenActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.LaunchScreen);
+
+            ImageView imageView = FindViewById<ImageView>(Resource.Id.animated_android);
+            AnimationDrawable animation = (AnimationDrawable)imageView.Drawable;
+            animation.Start();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Task.Run(async () => {
+                await Task.Delay(3000);
+                if (!IsFinishing)
+                {
+                    StartActivity(typeof(MainActivity));
+                    OverridePendingTransition(Resource.Animation.fade_in, Resource.Animation.fade_out);
+                }
+            });
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Finish();
+        }
+    }
+}
